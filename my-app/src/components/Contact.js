47c4eade34225dc21f6fd83938c944ff67e7b6dc
jellington22 <52639636+jellington22@users.noreply.github.com>
2021-08-27 10:48:59 -0400

    commit

---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Contact = (props) => {
+  //    setTimeout(()=>{
+  //        props.history.push('/about')
+  //    }, 2000)
+
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum facilis
+        quo, ratione commodi accusamus debitis earum quis praesentium saepe,
+        reprehenderit nihil, minima corrupti exercitationem cupiditate! Quasi ab
+        enim repellat blanditiis?
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
